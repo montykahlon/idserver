9a7642cc75e94582e551ff64a51d9b5370a28850
Monty Kahlon <68314229+montykahlon@users.noreply.github.com>
2023-08-15 20:51:43 +0000

    removed logs

--- server.js
+++ server.js
@@ -2,10 +2,8 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { addWaiting, searchWaiting } from "./db.js"; // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
-
 // Creating the Express app
 const app = express();
-
 // Define a route at '/hello'
 app.get('/id', (req, res) => {
     console.log(`got req ${req}`)
@@ -25,7 +23,6 @@ app.get('/id', (req, res) => {
                 })
         } else {
             const { _id } = x.value
-            console.log(_id)
             res.send(_id);
         }
     }).catch((y) => { console.log(y) })
