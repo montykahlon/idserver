ec7ff2289ce973e8e80e4bdba46d1c9b02e2e7dc
Monty Kahlon <68314229+montykahlon@users.noreply.github.com>
2023-08-15 03:36:31 +0000

    commit

---
+++ db.js
@@ -0,0 +1,31 @@
+import mongoose, { mongo } from "mongoose";
+//mongoose.connect("mongodb+srv://connect4emoji:na!%237gng!nag@cluster0.yidpscv.mongodb.net/connect4").then(x => console.log(x.version))
+const conn = mongoose.connection;
+
+export const addWaiting = (e) => {
+    return async () => {
+        try {
+            await mongoose.connect('mongodb+srv://connect4emoji:na!%237gng!nag@cluster0.yidpscv.mongodb.net/connect4');
+            let conn = mongoose.connection
+            let record = await conn.collection("waitingroom").insertOne(e)
+            return record.acknowledged
+            // Your code here
+        } catch (error) {
+            console.error('Mongoose connection error:', error);
+        }
+    }
+}
+
+export const searchWaiting = () => {
+    return async () => {
+        try {
+            await mongoose.connect('mongodb+srv://connect4emoji:na!%237gng!nag@cluster0.yidpscv.mongodb.net/connect4');
+            let conn = mongoose.connection
+            let delRecord = (await conn.collection("waitingroom").findOneAndDelete({}))
+            return delRecord
+            // Your code here
+        } catch (error) {
+            console.error('Mongoose connection error:', error);
+        }
+    }
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+// Importing required modules
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import { addWaiting, searchWaiting } from "./db.js"; // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+// Creating the Express app
+const app = express();
+
+// Define a route at '/hello'
+app.get('/id', (req, res) => {
+    console.log(`got req ${req}`)
+    searchWaiting()().then(x => {
+        console.log(x)
+        if (x.value === null) {
+            //! new game without a player waiting
+            const _id = uuidv4()
+            res.send(_id);
+            addWaiting({ _id: _id })()
+                .then((e) => {
+                    console.log(_id)
+                    res.send(_id);
+                })
+                .catch((y) => {
+                    console.log(y)
+                })
+        } else {
+            const { _id } = x.value
+            console.log(_id)
+            res.send(_id);
+        }
+    }).catch((y) => { console.log(y) })
+});
+// Define a default route to handle other requests
+app.get('*', (req, res) => {
+    res.status(404).send('Page not found');
+});
+
+// Set the server to listen on port 8080
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
