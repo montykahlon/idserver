1c8291c9010409b88297a74791ce1d5e293e610c
Monty Kahlon <68314229+montykahlon@users.noreply.github.com>
2025-03-16 13:45:55 -0400

    minor updates

--- db.js
+++ db.js
@@ -5,8 +5,8 @@ const conn = mongoose.connection;
 export const addWaiting = async (e) => {
         try {
             await mongoose.connect('mongodb+srv://connect4emoji:na!%237gng!nag@cluster0.yidpscv.mongodb.net/connect4');
-            let conn = mongoose.connection
-            let record = await conn.collection("waitingroom").insertOne(e)
+            const conn = mongoose.connection
+            const record = await conn.collection("waitingroom").insertOne(e)
             return record.acknowledged
             // Your code here
         } catch (error) {
@@ -17,11 +17,12 @@ export const addWaiting = async (e) => {
 export const searchWaiting = async () => {
         try {
             await mongoose.connect('mongodb+srv://connect4emoji:na!%237gng!nag@cluster0.yidpscv.mongodb.net/connect4');
-            let conn = mongoose.connection
-            let delRecord = (await conn.collection("waitingroom").findOneAndDelete({}))
+            const conn = mongoose.connection
+            const delRecord = (await conn.collection("waitingroom").findOneAndDelete({}))
             return delRecord
             // Your code here
         } catch (error) {
             console.error('Mongoose connection error:', error);
+            throw error
         }
 }
--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 // Importing required modules
 import express from 'express';
+import logger from './logger.js';
 import { v4 as uuidv4 } from 'uuid';
 import { addWaiting, searchWaiting } from "./db.js"; // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 // Creating the Express app
 const app = express();
 // Define a route at '/hello'
 app.get('/id', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
     searchWaiting().then(x => {
-        if (x.value === null) {
+        if (x === null) {
             //! new game without a player waiting
             const _id = uuidv4()
             addWaiting({ _id: _id })
                 .then((e) => {
-                    console.log("player1", _id)
-                    res.status(200).send(_id);
+                    res.status(200).send(`${_id}`);
                 })
                 .catch((y) => {
-                    console.log(y)
+                    logger.info(`the following error occured at addwaiting ${y}`) 
                 })
         } else {
-            const { _id } = x.value
+            const _id = x._id
             res.status(200).send(_id);
         }
-    }).catch((y) => { console.log(y) })
+    }).catch((y) => { logger.info(`the following error occured at search waiting ${y}`) })
 });
 // Define a default route to handle other requests
 app.get('*', (req, res) => {
@@ -30,7 +32,8 @@ app.get('*', (req, res) => {
 });
 
 // Set the server to listen on port 8080
-const port = 3001;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+const port = 5000;
+const host = '0.0.0.0';
+app.listen(port, host, () => {
+    logger.info(`Server is listening on port ${port}`);
 }); 
